669cb29205d6b03922c35dd1042394c2faefde7c
saranourmahmoud <161065103+saranourmahmoudal-tantawy@users.noreply.github.com>
2024-03-29 21:09:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,12 +49,18 @@ resource "aws_security_group" "sg" {
   }
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "CI/CD"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCTg1AM/HxGdq0iL0WApz5ytXuy4VPfm4lElWNyurRuOP3Q6uN39Pu5rv57AVOyivY9sFIc+qi0yuDmRR/OcVXBtYhZR8xSvevCOSgyNQ3uicEGVnjFyW1LUn8G9WiOkIA90hztXjunujUndDZBGFVgab+02MN5OmDeSpOjWIaYo7B4dBbfbiczV/WN+R6oevUcdLcn8A1pqV92UwNEu9EtqMLYei4i163BH0zOcjVPrAZwuWcuvth45RVwE4Q+YAQCR4RYwdrDWYBkHzDfiTr2sj+1lU01z0COUQXmu6XpQxzWkFmSyGUtsSYr383A49Yv1Ji5JDh3yTxWvBgWAkM9sYkBDi3PyzkOhuCBSDkG/6DRRRP/wrOMQjyK42xSdA+afRXRm9Y1gZoKbhGqWiOerLDNCPM4laV7IbpR2BBsMXEEenCH+4QMU07eoj8jSGoV3KtmLgUmz1/JIkrbBErgWvKVH7w54DbFJCPAC0ijswY9NOwuL2cgHJ0+sB1k1FU= sara@cloud"
+}
+
 # Creating EC2 instance 
 resource "aws_instance" "EC2" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = aws_subnet.demosubnet1.id
   vpc_security_group_ids = [ aws_security_group.sg.id ]
+  key_name      = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "Python-App"
