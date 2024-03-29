e8d0cad71d0640a262634c142dce2f8707072968
saranourmahmoud <161065103+saranourmahmoudal-tantawy@users.noreply.github.com>
2024-03-30 01:02:34 +0200

    Update main.py

--- my_package/main.py
+++ my_package/main.py
@@ -34,4 +34,4 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
 if __name__ == '__main__':
-    app.run(port=5000, host="0.0.0.0", debug=False)
+    app.run(port=5000, host="0.0.0.0", debug=True)
